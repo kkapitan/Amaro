6958c73ff767fafa7ae6941cb31b19eb11def995
Krzysztof <krzysztof.kapitan@gmail.com>
2016-05-28 20:46:46 +0200

    Update sources

--- CrushBootstrap/Application/CRBSAppDelegate.m
+++ CrushBootstrap/Application/CRBSAppDelegate.m
@@ -8,43 +8,11 @@
 
 #import "CRBSAppDelegate.h"
 
-#if HAS_POD(CocoaLumberjack)
-    #import <CocoaLumberjack/CocoaLumberjack.h>
-
-    #if HAS_POD(Sidecar)
-    #import <Sidecar/CRLMethodLogFormatter.h>
-    #endif
-#endif
-
-
 @implementation CRBSAppDelegate
 
 -(BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    [self initializeLogging];
-
     return YES;
-}
-
-
-#pragma mark Amaro foundation goodies
-
-/**
- Sets up CocoaLumberjack
- */
--(void)initializeLogging
-{
-    #if HAS_POD(CocoaLumberjack)
-        #if HAS_POD(Sidecar)
-        CRLMethodLogFormatter *logFormatter = [[CRLMethodLogFormatter alloc] init];
-        [[DDASLLogger sharedInstance] setLogFormatter:logFormatter];
-        [[DDTTYLogger sharedInstance] setLogFormatter:logFormatter];
-        #endif
-
-        // Emulate NSLog behavior for DDLog*
-        [DDLog addLogger:[DDASLLogger sharedInstance]];
-        [DDLog addLogger:[DDTTYLogger sharedInstance]];
-    #endif
 }
 
 @end
